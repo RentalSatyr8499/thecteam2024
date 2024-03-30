23a091367a203a151b17858493403f0cf93961c6
RentalSatyr8499 <87041733+RentalSatyr8499@users.noreply.github.com>
2024-03-30 19:03:05 -0400

    trying the califronia map
    wheee!!

--- fire risk map/fire risk map.py
+++ fire risk map/fire risk map.py
@@ -1,22 +1,45 @@
-from mpl_toolkits.basemap import Basemap
-import matplotlib.pyplot as plt
+from mpl_toolkits.basemap import Basemap, shiftgrid, cm
 import numpy as np
+import matplotlib.pyplot as plt
+from netCDF4 import Dataset
 
-# Set up orthographic map projection
-map = Basemap(projection='ortho', lat_0=45, lon_0=-100, resolution='l')
+# read in etopo5 topography/bathymetry.
+url = 'http://ferret.pmel.noaa.gov/thredds/dodsC/data/PMEL/etopo5.nc'
+etopodata = Dataset(url)
 
-# Draw coastlines, country boundaries, and fill continents
-map.drawcoastlines(linewidth=0.25)
-map.drawcountries(linewidth=0.25)
-map.fillcontinents(color='coral', lake_color='aqua')
+topoin = etopodata.variables['ROSE'][:]
+lons = etopodata.variables['ETOPO05_X'][:]
+lats = etopodata.variables['ETOPO05_Y'][:]
+# shift data so lons go from -180 to 180 instead of 20 to 380.
+topoin,lons = shiftgrid(180.,topoin,lons,start=False)
 
-# Draw the edge of the map projection region
-map.drawmapboundary(fill_color='aqua')
+# plot topography/bathymetry as an image.
 
-# Draw lat/lon grid lines every 30 degrees
-map.drawmeridians(np.arange(0, 360, 30))
-map.drawparallels(np.arange(-90, 90, 30))
-
-# Show the plot
-plt.title('Contour lines over filled continent background')
+# create the figure and axes instances.
+fig = plt.figure()
+ax = fig.add_axes([0.1,0.1,0.8,0.8])
+# setup of basemap ('lcc' = lambert conformal conic).
+# use major and minor sphere radii from WGS84 ellipsoid.
+m = Basemap(llcrnrlon=-145.5,llcrnrlat=1.,urcrnrlon=-2.566,urcrnrlat=46.352,\
+            rsphere=(6378137.00,6356752.3142),\
+            resolution='l',area_thresh=1000.,projection='lcc',\
+            lat_1=50.,lon_0=-107.,ax=ax)
+# transform to nx x ny regularly spaced 5km native projection grid
+nx = int((m.xmax-m.xmin)/5000.)+1; ny = int((m.ymax-m.ymin)/5000.)+1
+topodat = m.transform_scalar(topoin,lons,lats,nx,ny)
+# plot image over map with imshow.
+im = m.imshow(topodat,cm.GMT_haxby)
+# draw coastlines and political boundaries.
+m.drawcoastlines()
+m.drawcountries()
+m.drawstates()
+# draw parallels and meridians.
+# label on left and bottom of map.
+parallels = np.arange(0.,80,20.)
+m.drawparallels(parallels,labels=[1,0,0,1])
+meridians = np.arange(10.,360.,30.)
+m.drawmeridians(meridians,labels=[1,0,0,1])
+# add colorbar
+cb = m.colorbar(im,"right", size="5%", pad='2%')
+ax.set_title('ETOPO5 Topography - Lambert Conformal Conic')
 plt.show()
