f44fa9ebd47d9ab86dc85159c20e5c35dc41abe7
RentalSatyr8499 <87041733+RentalSatyr8499@users.noreply.github.com>
2024-03-31 09:12:58 -0400

    WE COLORED A COUNTY!!!
    THIS CODE COLORS ALAMEDA IN RED!!!! WHOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO

--- fire risk map/fire risk map.py
+++ fire risk map/fire risk map.py
@@ -5,8 +5,18 @@ import matplotlib.pyplot as plt
 shapefile_path = r'C:\Users\ychun\Downloads\tl_2023_us_county\tl_2023_us_county.shx'
 gdf = gpd.read_file(shapefile_path)
 
-# Plot the county boundaries
-gdf.plot(edgecolor='black', linewidth=0.5)
+# Filter the GeoDataFrame to get only the California counties (with 'STATEFP' value '06')
+california_gdf = gdf[gdf['STATEFP'] == '06']
+
+# Create a plot for California counties in blue
+ax = california_gdf.plot(edgecolor='black', linewidth=0.5)
+
+# Filter the GeoDataFrame to get only the Alameda county
+alameda_gdf = california_gdf[california_gdf['NAME'] == 'Alameda']
+
+# Plot the Alameda county in red (if it exists in the filtered dataset)
+if not alameda_gdf.empty:
+    alameda_gdf.plot(ax=ax, color='red', edgecolor='black', linewidth=1)
 
 # Customize the plot (add title, labels, etc.)
 plt.title('California County Boundaries with Alameda County Highlighted')
